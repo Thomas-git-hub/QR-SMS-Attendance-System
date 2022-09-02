1f531f496c0dd929b2e89a6f41b6a304c9645acb
maryjoyelquiero <joyelquiero2525@gmail.com>
2022-09-02 12:14:03 +0800

    Student side

---
+++ view/student/includes/sidenav.php
@@ -0,0 +1,34 @@
+
+  <div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bx-qr-scan icon'></i>
+        <div class="logo_name">Attendance System</div>
+        <i class='bx bx-menu' id="btn" ></i>
+    </div>
+    <div class="nav-list">
+      
+      <li>
+       <a href="#">
+         <i class='bx bx-table' ></i>
+         <span class="links_name">Attendance Status</span>
+       </a>
+       <span class="tooltip">Attendance Status</span>
+     </li>
+     <li>
+       <a href="#">
+       <i class='bx bxs-file'></i>
+         <span class="links_name">Activities</span>
+       </a>
+       <span class="tooltip">Activities</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-cog' ></i>
+         <span class="links_name">Settings</span>
+       </a>
+       <span class="tooltip">Settings</span>
+     </li>
+   
+    </div>
+  </div>
+
